59a000f3149a67e89a26ef4d663fc2ce249ba72e
Safal Gautam <saphal106@gmail.com>
2025-06-08 10:58:07 +0545

    Optimized

--- main.py
+++ main.py
@@ -18,11 +18,18 @@ def load_movies():
         with open(FILE, 'w') as file:
             return []
 
-def search_movies(title):
+def details_title(title):
     params = {'s': title, 'apikey': api_key}
-    response = requests.get(url, params=params).json()
+    return requests.get(url, params=params).json()
+    
+def details_id(id):
+    params = {'i': id, 'apikey': api_key}
+    return  requests.get(url, params=params).json()
 
-    if response.get("Response") == "True":
+def search_movies(title):
+    response = details_title(title)
+
+    if response :
         for i, result in enumerate(response["Search"], start=1):
             print(f"{i}.\tTitle: {result['Title']} \n\tYear: {result['Year']}")
             imdb_ids.append(result["imdbID"])
@@ -30,10 +37,9 @@ def search_movies(title):
         print(f"No results found for '{title}'")
 
 def get_movie_details(imdb_id):
-    params = {'i': imdb_id, 'apikey': api_key}
-    response = requests.get(url, params=params).json()
+    response = details_id(imdb_id)
 
-    if response.get("Response") == "True":
+    if response:
         print("\n🎬 Movie Details:\n")
         print(f"Title       : {response.get('Title', 'N/A')}")
         print(f"Year        : {response.get('Year', 'N/A')}")
@@ -45,15 +51,18 @@ def get_movie_details(imdb_id):
         print(f"Could not fetch details for ID {imdb_id}")
 
 def get_movie(title, year):
-    params = {'s': title, 'apikey': api_key}
-    response = requests.get(url, params=params).json()
+    response = details_title(title)
 
-    if response.get("Response") == "True":
+    if response:
         for result in response['Search']:
             if (result['Year'] == year):
                 get_movie_details(result['imdbID'])
 
+    else:
+        print("Failed")
 
+def add_movie(title, year):
+    response 
 
 def main():
     parser = argparse.ArgumentParser(
@@ -72,15 +81,11 @@ def main():
 
     # 'add' command
     parser_add = sub_parser.add_parser("add", help="Add movie to vault")
-    parser_add.add_argument("--title", required=True)
-    parser_add.add_argument("--director", required=True)
+    parser_add.add_argument("--title", required=True, help="Title of movie")
+    parser_add.add_argument("--year", required=True, help="Year of release of movie")
 
     # 'list' command
     parser_list = sub_parser.add_parser("list", help="List all saved movies")
-
-    # 'view' command
-    parser_view = sub_parser.add_parser("view", help="View info of saved movie")
-    parser_view.add_argument("--title", required=True)
 
     # 'remove' command
     parser_remove = sub_parser.add_parser("remove", help="Remove a saved movie")
@@ -94,6 +99,9 @@ def main():
 
     elif args.command == "details":
         get_movie(args.title, args.year)
+
+    elif args.command == "add":
+        add_movie(args.title, args.director)
 
 
 if __name__ == "__main__":
